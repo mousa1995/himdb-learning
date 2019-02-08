aa42a9007cf509cbfa06c18d694cddbd4d5ff70b
mousa1995 <mousasoccerlove@gmail.com>
2019-02-08 18:08:11 +0330

    اگه گفتی اشتباه چیه؟

---
+++ src/components/elements/SearchBar/SearchBar.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './SearchBar.css';
+import FontAwsome from 'react-fontawesome';
+
+class SearchBar extends React.Component {
+  state{
+
+  }
+  render(){
+    return (
+      <div>search bar</div>
+    )
+  }
+}
