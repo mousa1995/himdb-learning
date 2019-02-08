4d2b90683363d25a100fdee473a34425af85e700
mousa1995 <mousasoccerlove@gmail.com>
2019-02-08 17:08:39 +0330

    کامنتو بخون

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -7,13 +7,16 @@ class Home extends React.Component {
 
   }
 
-  render(
+  render(){
     return (
-    
+      <div>Home</div>
     );
-  ); 
+  }
 
 }
 
 export default Home;
-
+//ساختار نهایی خانه
+// کلاس است و استیت دارد
+//چیزی را رندر و رندر چیزی را ریترن میکند
+// نهایتا اکسپورت دیفالت میشود
