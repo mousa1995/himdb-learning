1fd4d7b0961fb1ff0569cab38dab3f18224be584
mousa1995 <mousasoccerlove@gmail.com>
2019-02-08 16:48:04 +0330

    اضافه کردن فایل رندر شونده

--- src/index.js
+++ src/index.js
@@ -2,4 +2,8 @@ import React from 'react';
 import ReactDom from 'react-dom';
 //ishoon baraye render kardane html estefade msihe 
 //mamulan toye index.js estefasde msihe
+import App from './components/App/App';
+//in hamun chizi ast ke render mishe 
+//این فایل وضیفه رندر کردن اصلی را به عهده داره
+
 
