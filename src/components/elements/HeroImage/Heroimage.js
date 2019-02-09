ece48d47555a33b880ac67bfe040cb19c5f2c037
mousa1995 <mousasoccerlove@gmail.com>
2019-02-09 18:45:39 +0330

    نمونه کد استایل داده شده در داخل ریاکت

--- src/components/elements/HeroImage/Heroimage.js
+++ src/components/elements/HeroImage/Heroimage.js
@@ -3,9 +3,16 @@ import './HeroImage.css';
 const HeroImage = (props) => {
   return (
     <div className="rmdb-heroimage" style={{
-      
+      background: `background: linear-gradient(to bottom,
+                                  rgba(0,0,0,0) 39%,
+                                  rgba(0,0,0,0) 41%,
+                                  rgba(0,0,0,.65) 100%),
+                                  url('${props.image}'), #1c1c1c;`
     }}
-    >Hero image</div>
+    >
+    
+      {/*container*/}
+    </div>
   );
 }
 
