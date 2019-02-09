cb109574253390a2ed14c9fa2fe84268ac2f1481
mousa1995 <mousasoccerlove@gmail.com>
2019-02-09 19:01:30 +0330

    سی اس اس درست شد

--- src/components/elements/HeroImage/Heroimage.js
+++ src/components/elements/HeroImage/Heroimage.js
@@ -3,16 +3,18 @@ import './HeroImage.css';
 const HeroImage = (props) => {
   return (
     <div className="rmdb-heroimage" style={{
-      background: `background: linear-gradient(to bottom,
-                                  rgba(0,0,0,0) 39%,
-                                  rgba(0,0,0,0) 41%,
-                                  rgba(0,0,0,.65) 100%),
-                                  url('${props.image}'), #1c1c1c;`
+      background: 
+        `linear-gradient(to bottom, rgba(0,0,0,0)
+        39%, rgba(0,0,0,0)
+        41%, rgba(0,0,0,0.65)
+        100%),
+        url('${props.image}'), #1c1c1c`
     }}
     >
     <div className="rmdb-heroimage-content">
       <div className="rmdb-heroimage-text">
-        <h1>${}</h1>
+        <h1>{props.title}</h1>
+        <p>{props.text}</p>
       </div>
     </div>
       {/*container*/}
