74aa4cdb368705956a89eaa7bf8d22d7df7b1226
mousa1995 <mousasoccerlove@gmail.com>
2019-02-09 20:42:02 +0330

    فونت اوسام

--- src/components/elements/SearchBar/SearchBar.js
+++ src/components/elements/SearchBar/SearchBar.js
@@ -5,12 +5,18 @@ import FontAwsome from 'react-fontawesome';
 class SearchBar extends React.Component {
   
   state = {
-
+    value: '',
   }
   
   render(){
     return (
-      <div>search bar</div>
+      <div className="rmdb-searchbar">
+        <div className="rmdb-searchbar-content">
+          <FontAwsome className="rmdb-fa-search" name="search"/>
+          {/* نامی که در فونت اوسام رد شده مال خود فونت اوسام است و کلاس رو خودش نوشته*/}
+          
+        </div>
+      </div>
     )
   }
 }
