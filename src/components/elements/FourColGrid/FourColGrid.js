eadbfcc907aed637c8c605f704d1c105993a6ffa
mousa1995 <mousasoccerlove@gmail.com>
2019-02-10 13:39:19 +0330

    خط هفتم چرا کانست را قبول کرد ؟ کجا قبول نمیکرد یادته؟

--- src/components/elements/FourColGrid/FourColGrid.js
+++ src/components/elements/FourColGrid/FourColGrid.js
@@ -3,8 +3,19 @@ import './FourColGrid.css';
 
 
 const FourColGrid = (props) => {
+
+  const renderElements =  () => {
+    
+  }
+
   return (
-    <div>FourColGrid</div>
+    <div className="rmdb-grid">
+      {/* کد زیر میگه لودینگ نباشه و هدری باشه نشونش بده */}
+      { !loading && props.header ? <h1>{props.header}</h1> : null }
+      <div className="rmdb-grid-content">
+        {renderElements()}
+      </div>
+    </div>
   );
 }
 
