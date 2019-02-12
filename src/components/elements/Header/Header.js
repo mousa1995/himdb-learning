9f8aa7f96d6db9e2b4b4e776701fe05e030f68df
mousa1995 <mousasoccerlove@gmail.com>
2019-02-12 14:34:40 +0330

    نمی فهمم چی به چیه

--- src/components/elements/Header/Header.js
+++ src/components/elements/Header/Header.js
@@ -1,12 +1,15 @@
 //نمونه یک فانکشنال کامپوننت یا بدون استیت است
 import React from  'react';
 import './Header.css';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
   return (
     <div className="rmdb-header">
       <div className="rmdb-header-content">
-        <img src="./images/reactMovie_logo.png" alt="imdb-logo" className="rmdb-logo"/>
+        <Link to="/">
+          <img src="./images/reactMovie_logo.png" alt="imdb-logo" className="rmdb-logo"/>
+        </Link>
         <img src="./images/tmdb_logo.png" alt="tmdb-logo" className="rmdb-tmdb-logo"/>
       </div>
     </div>
--- src/components/elements/MovieThumb/MovieThumb.js
+++ src/components/elements/MovieThumb/MovieThumb.js
@@ -1,10 +1,19 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './MovieThumb.css';
+
 
 const MovieThumb = (props) => {
   return (
-    <div>
-      <img src={props.image} alt="MovieTumb"/>
+    <div className="rmdb-moviethumb">
+      {props.clickable 
+        ? 
+        <Link to={{ pathname: `/${props.movieId}`, movieName: `${props.movieName}` }}>
+        <img src={props.image} alt="MovieTumb"/>
+        </Link>
+        :
+        <img src={props.image} alt="MovieTumb"/>
+      }
     </div>
   );
 }
