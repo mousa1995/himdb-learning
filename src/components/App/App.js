3b66d1ca41035d11438263ca3c2f75166e95457d
mousa1995 <mousasoccerlove@gmail.com>
2019-02-12 13:58:00 +0330

    بعد از ساختار اولیه لینک ها رو میسازیم

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Router, Route ,  BrowserRouter, Switch} from 'react-router-dom';
 import Header from  '../elements/Header/Header';
 import Home from '../Home/Home';
+import Movie from '../Movie/Movie';
 import NotFound from '../elements/NotFound/NotFound';
 
 
