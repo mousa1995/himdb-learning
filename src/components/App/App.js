c78e224ce5d95bdf530d07c423dab2d054243b88
mousa1995 <mousasoccerlove@gmail.com>
2019-02-12 12:04:05 +0330

    مقدمات روتر رو وارد کردیم

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,14 +1,14 @@
 import React from 'react';
+import { Route, Router, BrowserRouter } from 'react-router-dom';
 import Header from  '../elements/Header/Header';
 import Home from '../Home/Home';
 
 
 const App = () => {
   return (
-    <React.Fragment>
-      <Header />
-      <Home />
-    </React.Fragment>
+    <div>
+      
+    </div>
   );
 }
 
