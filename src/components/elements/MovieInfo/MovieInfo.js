136aa22c5b033a9062d49d918363614f4aac207e
mousa1995 <mousasoccerlove@gmail.com>
2019-02-12 17:03:27 +0330

    ساختار اولیه زیاد مهم نیست

---
+++ src/components/elements/MovieInfo/MovieInfo.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { BACKDROP_SIZE, IMAGE_BASE_URL, POSTER_SIZE } from '../../../config';
+import FontAwesome from 'react-fontawesome';
+import MovieThumb from '../MovieThumb/MovieThumb';
+import './MovieInfo.css';
+
+
+const MovieInfo = (props) => {
+
+  return (
+    <div>
+    MovieInfo works!
+    </div>
+  )
+
+}
+
+export default MovieInfo;
