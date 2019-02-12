c9a4d7a857c8cfe16bac3d9a933479938f50b5aa
mousa1995 <mousasoccerlove@gmail.com>
2019-02-12 12:17:44 +0330

    روتر خوانده شود

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,14 +1,22 @@
 import React from 'react';
-import { Route, Router, BrowserRouter } from 'react-router-dom';
+import { Router, Route ,  BrowserRouter, Switch} from 'react-router-dom';
 import Header from  '../elements/Header/Header';
 import Home from '../Home/Home';
 
 
 const App = () => {
   return (
-    <div>
-      
-    </div>
+    <BrowserRouter>
+      <React.Fragment>
+      <Header/>
+      <Switch>
+      <Route path="/" component={Home} exact/>
+      <Route path="/:movieId" component={Movie} exact/>
+      <Route component={NotFound}/>
+
+      </Switch>
+      </React.Fragment>
+    </BrowserRouter>
   );
 }
 
