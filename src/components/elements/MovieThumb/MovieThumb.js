0b739f4643e5004e74c7e0688c27737c39455a13
Mousa__Rm <mousasoccerlove@gmail.com>
2019-03-03 18:27:00 +0330

    دیستراکت کردن فانکشنال کامپوننت ها

--- src/components/elements/MovieThumb/MovieThumb.js
+++ src/components/elements/MovieThumb/MovieThumb.js
@@ -4,20 +4,25 @@ import Proptypes from 'prop-types';
 import './MovieThumb.css';
 
 
-const MovieThumb = (props) => {
+const MovieThumb = ({ image, clickable, movieId, movieName }) => {
   return (
     <div className="rmdb-moviethumb">
-      {props.clickable 
+      {clickable 
         ? 
-        <Link to={{ pathname: `/${props.movieId}`, movieName: `${props.movieName}` }}>
-        <img src={props.image} alt="MovieTumb"/>
+        <Link to={{ pathname: `/${movieId}`, movieName: `${movieName}` }}>
+        <img src={image} alt="MovieTumb"/>
         </Link>
         :
-        <img src={props.image} alt="MovieTumb"/>
+        <img src={image} alt="MovieTumb"/>
       }
     </div>
   );
 }
+
+//روش دیستراکت کردن پراپ های برای فانکشنال کامپوننت ها
+//برای کلاس ها تفاوت میکنه
+//پشت تموم چیز هایی که پراپ دات بود ور میداریم و فقط اسم رو میگذاریم
+//مثل قبل نیازی نیست موقع دیستراکت کردن بگیم از کجا فقط مثل بالا یک اکولاد باز میکنیم و دیستراکت میکنیم
 
 //این چیزی ک الان ازش استفاده کردیم بهش پراپ تایپ میگن چک میکنه که پراپ درست برای کامپوننت فرستاده بشه
 //میایم قبل صادرات مینویسیمش
