8dc38b123db897238f9c837541ee9acb16d5156d
Mousa__Rm <mousasoccerlove@gmail.com>
2019-03-03 14:38:25 +0330

    روش استفاده فانکشن

--- src/components/elements/MovieInfoBar/MovieInfoBar.js
+++ src/components/elements/MovieInfoBar/MovieInfoBar.js
@@ -5,8 +5,23 @@ import './MovieInfoBar.css';
 
 const MovieInfoBar = (props) => {
   return (
-    <div>
-    MovieInfoBar works!
+    <div className="rmdb-movieinfobar">
+      <div className="rmdb-movieinfobar-content">
+        <div className="rmdb-movieinfobar-content-col">
+          <FontAwesome className="fa-time" name="clock" size="2x"/>
+          <span className="rmdb-movieinfobar-info">Running time: {calcTime(props.time)}</span>
+        </div>
+
+        <div className="rmdb-movieinfobar-content-col">
+          <FontAwesome className="fa-budget" name="money" size="2x"/>
+          <span className="rmdb-movieinfobar-info">Budget : {convertMoney(props.budget)}</span>
+        </div>
+
+        <div className="rmdb-movieinfobar-content-col">
+          <FontAwesome className="fa-revenue" size="2x" name="ticket"/>
+          <span className="rmdb-movieinfobar-info">Revenue: {convertMoney(props.revenue)}</span>
+        </div>
+      </div>
     </div>
   )
 }
