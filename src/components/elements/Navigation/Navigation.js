efcc87678607f445a7805c5a1b2a3e8f4706e5f9
Mousa__Rm <mousasoccerlove@gmail.com>
2019-03-03 11:52:13 +0330

    ساختار اولیه جهت یاب برای صفحه تک فیلم

--- src/components/elements/Navigation/Navigation.js
+++ src/components/elements/Navigation/Navigation.js
@@ -3,7 +3,17 @@ import { Link } from 'react-router-dom';
 
 const Navigation = (props) => {
   return (
-    <div>Navigation works!</div>
+    <div className="rmdb-navigation">
+      <div className="rmdb-navigation-content">
+        <link to="/">
+        <p>home</p>
+        </link>
+        <p>/</p>
+
+        <p>{props.movie}</p>
+        {/* توی بالا یی عنوان فیلم رو میگیریم */}
+      </div>
+    </div>
   )
 }
 
